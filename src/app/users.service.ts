7d26a35108d1ee28821d1df079f53f5d9e065998
daisymae <daisymae@orcadiafarm.com>
2018-04-28 21:09:46 -0500

    Add Services
    Added two services: a usersService and a counterService. The
    usersService took the place of the @Input/@Output/emitters to pass data
    between the components.
    The counterService just adds everytime a user is made inactive or
    active.

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Injectable()
 export class UsersService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  constructor() { }
+  constructor(private counter: CounterService) { }
 
   onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counter.addInactive();
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counter.addActive();
   }
 
 }
